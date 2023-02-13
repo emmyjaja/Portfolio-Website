40475984d789367ee335a21280f1327328cc8ea7
emmyjaja <emmykjohn@gmail.com>
2023-02-13 06:41:29 +0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+console.log('Hello world');
+
+//let, const
+
+let Age=30;
+
+console.log(age);
+
+//let, const
+
+let score=10
+
+console.log(score);
+//string,numbers,boolean,null,undefined
+
+const name='John';
+const age='30'
+const isCool='True'
+const x='nul';
+
+
+console.log()
+
